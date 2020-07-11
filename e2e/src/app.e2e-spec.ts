c5116c8ed62baf0ee0aec37d35a3c43329a72d54
Emilin DADIE <dadie.emilin@gmail.com>
2020-07-11 11:16:43 +0200

    scenario get title of home page which is article page KO cas

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display article title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('app app is running!');
+    expect(page.getInitialRouteTitleText()).toEqual('Article page');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,11 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getInitialRouteTitleText(): Promise<string> {
+    return element(by.css('app-root app-article h1')).getText() as Promise<string>;
+  }
+
+  navigateToCustomRoute(route: string) {
+    return browser.get(`${browser.baseurl}/${route}`);
   }
 }
